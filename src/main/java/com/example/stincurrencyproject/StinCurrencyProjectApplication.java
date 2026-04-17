1809f1e00a27a820c9beabd3f0e1a2c11ef61a32
Nazar Samborsky <nsamborskyy@gmail.com>
2026-04-17 20:36:14 +0200

    init

---
+++ src/main/java/com/example/stincurrencyproject/StinCurrencyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.stincurrencyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinCurrencyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinCurrencyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stincurrencyproject/StinCurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stincurrencyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinCurrencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
