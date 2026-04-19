73c77a3adfbe215307ae964fdddb93386d80e0f1
Nazar Samborsky <nsamborskyy@gmail.com>
2026-04-19 11:21:20 +0200

    Přidání základních modelů

---
+++ src/main/java/com/example/stincurrencyproject/model/ExchangeRate.java
@@ -0,0 +1,9 @@
+package com.example.stincurrencyproject.model;
+
+import java.time.LocalDateTime;
+
+public class ExchangeRate {
+    private String currency;
+    private Double rate;
+    private LocalDateTime date;
+}
